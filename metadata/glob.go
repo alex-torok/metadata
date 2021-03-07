a8ea56b39701578653d2f681634d4021b9f16b37
Alex Torok <alext9@gmail.com>
2021-03-06 22:37:04 -0500

    more tests pass

--- metadata/glob.go
+++ metadata/glob.go
@@ -17,14 +17,7 @@ func (g Glob) Match(str string) bool {
 func NewGlob(pattern string) (*Glob, error) {
 	builder := strings.Builder{}
 
-	components := strings.Split(pattern, "*")
-	for _, c := range components[:len(components)-1] {
-		builder.WriteString(regexp.QuoteMeta(c))
-		fmt.Println(regexp.QuoteMeta(c))
-		// Anything that isn't a path separator
-		builder.WriteString("[^/]*")
-	}
-	builder.WriteString(regexp.QuoteMeta(components[len(components)-1]))
+	handleDoubleStars(&builder, pattern)
 
 	regexPattern := fmt.Sprintf("^%s$", builder.String())
 	fmt.Println(regexPattern)
@@ -35,4 +28,24 @@ func NewGlob(pattern string) (*Glob, error) {
 	return &Glob{
 		re: re,
 	}, nil
+}
+
+func handleDoubleStars(builder *strings.Builder, pattern string) {
+	components := strings.Split(pattern, "**")
+	for _, c := range components[:len(components)-1] {
+		handleSingleStars(builder, c)
+		// Anything
+		builder.WriteString(".*")
+	}
+	handleSingleStars(builder, components[len(components)-1])
+}
+
+func handleSingleStars(builder *strings.Builder, pattern string) {
+	components := strings.Split(pattern, "*")
+	for _, c := range components[:len(components)-1] {
+		builder.WriteString(regexp.QuoteMeta(c))
+		// Anything that isn't a path separator
+		builder.WriteString("[^/]*")
+	}
+	builder.WriteString(regexp.QuoteMeta(components[len(components)-1]))
 }
